a287ef88603dd852e643c41db7b1a63954ec6929
Ngoc Tri <maitri221998@gmail.com>
2020-03-11 08:34:48 +0700

    user

--- src/Components/Commons/Header/menu_user.js
+++ src/Components/Commons/Header/menu_user.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { getUserData } from '../../../action/action'
 import { useTranslation } from 'react-i18next';
+import ModalBox from '../Modal';
 
 
 
@@ -9,20 +10,30 @@ const MenuUser = ({ history },props) => {
 
   const { t } = useTranslation();
   const user = useSelector(state=>state.user)
+
+  const showModal = (e) => {
+    e.preventDefault();
+    document.querySelector('.modal-box').style.transform = "none";
+    document.querySelector('.modal-box').style.zIndex = 11;
+    document.querySelector('.modal-box').style.opacity = 1;
+  }
   
   return (
-    <ul>
-      <li className="-left -usernot">
-        <a href="#" onClick={(e) => {
-          e.preventDefault();
-          sessionStorage.removeItem('userData')
-          window.location.reload()
-        }}>{t('button.logout')}</a>
-      </li>
-      <li className="-left -usernot">
-        <a href="#">{t(`header.hello`)+ user.name}</a>
-      </li>
-    </ul>
+    <React.Fragment>
+      <ul>
+        <li className="-left -usernot">
+          <a href="#" onClick={(e) => {
+            e.preventDefault();
+            sessionStorage.removeItem('userData')
+            window.location.reload()
+          }}>{t('button.logout')}</a>
+        </li>
+        <li className="-left -usernot">
+          <a href="#" onClick={showModal}>{t(`header.hello`)+ user.name}</a>
+        </li>
+      </ul>
+      <ModalBox user={user}></ModalBox>
+    </React.Fragment>
   )
 }
 
